9bea2f8205530b5a3058d806d2eca5d0603c888a
Max Duval <max.duval@theguardian.com>
2023-07-31 09:33:33 +0100

    feat(design): Guardian grid and fonts

--- pages/food/landing-page-1.tsx
+++ pages/food/landing-page-1.tsx
@@ -1,35 +1,126 @@
 import { css } from "@emotion/react";
-import { Button, Footer } from "@guardian/source-react-components";
+import {
+  Button,
+  Footer,
+  SvgGuardianLogo,
+} from "@guardian/source-react-components";
+import { fonts, from, palette } from "@guardian/source-foundations";
 import { Image } from "../../shared/Image";
+import Head from "next/head";
+
+const grid = css`
+  display: grid;
+
+  grid-template-columns:
+    0px
+    [content-start]
+    repeat(4, minmax(0, 1fr))
+    [content-end]
+    0px;
+
+  column-gap: 10px;
+
+  ${from.mobileLandscape} {
+    column-gap: 20px;
+  }
+
+  ${from.tablet} {
+    grid-template-columns:
+      minmax(0, 1fr)
+      [content-start]
+      repeat(12, 40px)
+      [content-end]
+      minmax(0, 1fr);
+  }
+
+  ${from.desktop} {
+    grid-template-columns:
+      minmax(0, 1fr)
+      [content-start]
+      repeat(12, 60px)
+      [content-end]
+      minmax(0, 1fr);
+  }
+  ${from.leftCol} {
+    grid-template-columns:
+      minmax(0, 1fr)
+      [content-start]
+      repeat(14, 60px)
+      [content-end]
+      minmax(0, 1fr);
+  }
+
+  ${from.wide} {
+    grid-template-columns:
+      minmax(0, 1fr)
+      [content-start]
+      repeat(16, 60px)
+      [content-end]
+      minmax(0, 1fr);
+  }
+`;
 
 const Home = () => (
   <>
-    <main
-      css={css`
-        display: flex;
-        flex-direction: column;
-      `}
-    >
-      <h1>Let’s get cooking</h1>
+    <Head>
+      <title>Basecamp | Guardian</title>
+    </Head>
 
-      <p>Some text about this page</p>
+    <header style={{ backgroundColor: palette.brand[400] }} css={grid}>
+      <div
+        css={css`
+          width: 225px;
+          grid-column: -4 / span 2;
+          justify-self: end;
 
-      <Button>Click me</Button>
+          ${from.desktop} {
+            width: 300px;
+            grid-column: -6 / span 3;
+          }
+        `}
+      >
+        <SvgGuardianLogo textColor={palette.neutral[100]} />
+      </div>
+    </header>
 
-      <Image
-        src={
-          "https://media.guim.co.uk/1a863deb14a607837e27466c05939aa98c410bb5/0_651_3717_3717/3717.jpg"
-        }
-        alt="Ottolenghi’s salad"
-        width={300}
-        height={300}
-      />
+    <main style={{ backgroundColor: palette.neutral[100] }} css={grid}>
+      <section
+        css={css`
+          grid-area: content;
+          font-family: ${fonts.body};
+          min-height: 70dvh;
+        `}
+      >
+        <h1
+          css={css`
+            font-family: ${fonts.headline};
+            font-weight: 500;
+          `}
+        >
+          Let’s get cooking!
+        </h1>
 
-      <ol>
-        <li>First thing</li>
-        <li>Second thing</li>
-        <li>Third thing</li>
-      </ol>
+        <p>Some text about this page…</p>
+
+        <Button>Click me</Button>
+
+        <br />
+
+        <Image
+          src={
+            "https://media.guim.co.uk/1a863deb14a607837e27466c05939aa98c410bb5/0_651_3717_3717/3717.jpg"
+          }
+          alt="Ottolenghi’s salad"
+          width={300}
+          height={300}
+        />
+
+        <ol>
+          <li>First thing</li>
+          <li>Second thing</li>
+          <li>Third thing</li>
+        </ol>
+      </section>
     </main>
     <Footer />
   </>
---
+++ shared/Fonts.tsx
@@ -0,0 +1,7 @@
+export const Fonts = () => (
+  /* this is slow - do not use it for production Guardian sites */
+  <link
+    href="https://assets.guim.co.uk/static/frontend/fonts/font-faces.css"
+    rel="stylesheet"
+  />
+);

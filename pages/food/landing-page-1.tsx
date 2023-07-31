83ab483c19722e2a4128485999218a776061f544
Max Duval <max.duval@theguardian.com>
2023-07-31 08:24:30 +0100

    feat(Image): set basic image component

--- pages/food/landing-page-1.tsx
+++ pages/food/landing-page-1.tsx
@@ -1,5 +1,6 @@
 import { css } from "@emotion/react";
 import { Button, Footer } from "@guardian/source-react-components";
+import { Image } from "../../shared/Image";
 
 const Home = () => (
   <>
@@ -14,6 +15,15 @@ const Home = () => (
       <p>Some text about this page</p>
 
       <Button>Click me</Button>
+
+      <Image
+        src={
+          "https://media.guim.co.uk/1a863deb14a607837e27466c05939aa98c410bb5/0_651_3717_3717/3717.jpg"
+        }
+        alt="Ottolenghi’s salad"
+        width={300}
+        height={300}
+      />
 
       <ol>
         <li>First thing</li>
